fe82a4c2871f5b9945a523b3b9f6f35e486e9aa3
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-11-04 22:52:57 +0530

    add mydata

---
+++ mydata.js
@@ -0,0 +1,29 @@
+export default
+    {
+        
+        name :'Bhaskar.',
+        headerTagline: [//Line 1 For Header
+                        'Hi 👋 You found me! I am Bhaskar Maity.',
+                        //Line 2 For Header
+                        'Digital designer ',
+                        //Line 3 For Header
+                        'from India'
+    ],
+        //Contact Email
+        contactEmail:'maitybhaskar2015@gmail.com',
+        // About Text 
+        abouttext: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        //aboutImage:'https://images.unsplash.com/photo-1521587765099-8835e7201186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+       
+       ShowAboutImage:true,//true or false 
+       //  Projects 
+       projects:[
+          
+
+                   
+        ],
+        social: [
+            
+           
+        ]
+    }
