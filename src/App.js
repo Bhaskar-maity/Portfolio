0157c180a1d68c6e64adeb60bb0c69afbe61d669
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-11-08 22:18:03 +0530

    navabar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import navbar from './components/navbar'
 function App() {
   return (
     <div className="App">
-      
+      <navbar/>
     </div>
   );
 }
