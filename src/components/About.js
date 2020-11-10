81ba0471f1e0eb454e657ed79e911174d706e4c3
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-11-10 23:12:01 +0530

    about componenet add

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import data from '../mydata';
+
+class About extends Component {
+    state = {  }
+    render() { 
+        return (<div className='about'>
+            <div className='about-content'>
+            <h1>About.</h1>
+            
+                <p>{data.abouttext}</p>
+            
+            </div>
+            {data.ShowAboutImage ? <img src={data.aboutImage} alt='about iamge'></img> : null}
+            
+
+        </div>  );
+    }
+}
+ 
+export default About;
