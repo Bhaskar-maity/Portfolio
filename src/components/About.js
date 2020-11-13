16efa4504cb095c93d7f223a668bd1c1b0b4d16b
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-11-13 11:24:58 +0530

    about style change

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import  'react-bootstrap'
 import data from '../mydata';
 
 class About extends Component {
     state = {  }
     render() { 
-        return (<div className='about'>
-            <div className='about-content'>
-            <h1>About.</h1>
-            
-                <p>{data.abouttext}</p>
-            
+        return (
+        <div className='container'>
+
+            <div className='row about'>
+                <div className='about-content col-md-6'>
+                    <h1 className='pb-5'>About.</h1>
+                    
+                    <p>{data.abouttext}</p>
+                
+                </div>
+
+                <div className='col-md-6 '>
+                    {data.ShowAboutImage ? <img src={data.aboutImage} alt='about iamge'></img> : null}
+                </div>
             </div>
-            {data.ShowAboutImage ? <img src={data.aboutImage} alt='about iamge'></img> : null}
-            
 
         </div>  );
     }
