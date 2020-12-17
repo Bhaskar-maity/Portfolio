906879b33275adf6c3cc0c7b26b58a4128197316
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-18 00:05:41 +0530

    nav change

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Navbar,FormControl,Nav,Button, Form} from 'react-bootstrap';
+import {Navbar,FormControl,Nav,Button, Form, NavDropdown} from 'react-bootstrap';
 
 function NavControl() {
 
     return (
     <>
-      <Navbar bg="light" variant="light">
+      {/* <Navbar bg="light" variant="light">
       <Navbar.Brand href="#home">Bhaskar Maity</Navbar.Brand>
+      
       <Nav className="ml-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#features">About</Nav.Link>
@@ -14,7 +15,22 @@ function NavControl() {
         <Nav.Link href="#pricing">Catch me</Nav.Link>
       </Nav>
       
-    </Navbar>
+    </Navbar> */}
+
+<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ml-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#features">About</Nav.Link>
+        <Nav.Link href="#pricing">Projects</Nav.Link>
+        <Nav.Link href="#pricing">Catch me</Nav.Link>
+      
+    </Nav>
+    
+  </Navbar.Collapse>
+</Navbar>
     </>
     )
 }
