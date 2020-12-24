e6fbcfce4495ec0dc57e77656edf6603066d2d8f
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-24 10:23:59 +0530

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,17 +6,19 @@ import myImg from '../images/MyImg1.jpg';
 const About =()=> {
     
         return (
-        <div className='container'>
-
+        <div className='container '>
+            <h1 className='pb-5 text-center'>About.</h1>
+            <hr class="ribbon"/>
+            <hr class="ribbon-little"/>
             <div className='row about'>
                 <div className='about-content col-md-6'>
-                    <h1 className='pb-5'>About.</h1>
+                    
                     
                     <p>{data.abouttext}</p>
                 
                 </div>
 
-                <div className='col-md-6 '>
+                <div className='col-md-6 about-img'>
                     {data.ShowAboutImage ? <img src={myImg} alt='about iamge' className='img-fluid'></img> : null}
                 </div>
             </div>
